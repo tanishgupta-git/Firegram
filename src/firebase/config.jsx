ac87a1dd27c675a4f54295322f5f6dd691370633
tanishgupta-git <tanishgupta8181@gmail.com>
2021-05-23 15:48:27 +0530

    added .env file for firebase api keys

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -3,14 +3,14 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCMQSeZX0UKqi5EkmK3X60aqCsVK6lNhCM",
-    authDomain: "fir-firestore-demo-76f56.firebaseapp.com",
-    databaseURL: "https://fir-firestore-demo-76f56.firebaseio.com",
-    projectId: "fir-firestore-demo-76f56",
-    storageBucket: "fir-firestore-demo-76f56.appspot.com",
-    messagingSenderId: "13197049093",
-    appId: "1:13197049093:web:de62ad94d5538fb8564b6c",
-    measurementId: "G-MPG05ZPFS4"
+    apiKey:process.env.REACT_APP_apiKey,
+    authDomain:process.env.REACT_APP_authDomain,
+    databaseURL:process.env.REACT_APP_databaseURL,
+    projectId:process.env.REACT_APP_projectId,
+    storageBucket:process.env.REACT_APP_storageBucket,
+    messagingSenderId:process.env.REACT_APP_messagingSenderId,
+    appId:process.env.REACT_APP_appId,
+    measurementId:process.env.REACT_APP_measurementId
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
